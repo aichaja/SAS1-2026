9b732ce30a58fcc2e0203f0c989be206af8ab34f
Aicha <aichaj2@gmail.com>
2026-09-04 21:22:28 +0100

    Compter les diviseurs

--- day2/boucle/Challenge12.js
+++ day2/boucle/Challenge12.js
@@ -1,9 +1,15 @@
 const prompt = require("prompt-sync")();
 
-let nombre = Number(prompt("Entre Nomber :"))
-
-for(i = 1; nombre >= i;i++){
+let nombre = Number(prompt("Nomber :"))
+console.log("Diviseurs :")
+let total = 0
+for(let i = 1; nombre >= i;i++){
     if(nombre % i == 0){
+        
         console.log(i)
+        total = total + 1
+        
     }
+    
 }
+console.log("Nombre de diviseurs :",total)
