544c10a00d7cbf6715f851577f19a0a167e695e1
aichajalloul <aichajalloul51@gmail.com>
2026-09-23 13:23:52 +0000

    Recherche d'Élément (Simulation de includes)

---
+++ day5/tableaux/Challenge5.js
@@ -0,0 +1,12 @@
+function contientElement(tab, valeur){
+    for(let i = 0; i < tab.length; i++){
+        if(tab[i] === valeur){
+            return true
+        }
+        
+    }
+    return false
+    
+    
+}
+console.log(contientElement(["pomme", "banane"], "banane"))
